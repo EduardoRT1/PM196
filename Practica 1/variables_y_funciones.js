5f76409ee9f6e1ee304f31ab02a459574d905c8b
EduardoRT1 <122043716@upq.edu.mx>
2025-05-16 17:27:23 -0600

    Creacion del archivo y problema a
    usamos let en el nombre para poder cambiarlo despues a maria, utilizamos el const para la edad ya que no cambia en este contexto y por ultimo let para guardar el mensaje

---
+++ Practica 1/variables_y_funciones.js
@@ -0,0 +1,7 @@
+// Variables y funciones en JavaScript
+let nombre = "Armando" ;
+const edad = 25;
+
+nombre = "Ana Mariana";
+let saludo = "Hola, " + nombre + ", tienes " + edad + " años";
+console.log(saludo);
