951b69ba9f61fcfb7e8bfb795270b5072cc3183b
EduardoRT1 <122043716@upq.edu.mx>
2025-05-16 17:52:19 -0600

    ejercicio b: Seguimmos la estructura de un arrow, parametros + => + el proceso de la función

--- Practica 1/variables_y_funciones.js
+++ Practica 1/variables_y_funciones.js
@@ -5,3 +5,10 @@ const edad = 25;
 nombre = "Ana Mariana";
 let saludo = "Hola, " + nombre + ", tienes " + edad + " años";
 console.log(saludo);
+
+//funcion arrow para el cuadrado de un numero
+const cuadrado = (num) => {
+    return num * num;
+}
+console.log(cuadrado(10));
+
