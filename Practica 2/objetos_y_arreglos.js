e7ef7d9e7bb5a0b4cc1d7394081f3c989e1f775c
EduardoRT1 <122043716@upq.edu.mx>
2025-05-20 17:35:58 -0600

    EJERCICIO C: Cada petición es separada por lo que primero buscamos a personaLuis dentro del arreglo persona, indicamos que buscamos la cadena “luis”. Seguimos con el nombre y edad, por utlimo sumamos todas la edades con reduce y el acumulador

--- Practica 2/objetos_y_arreglos.js
+++ Practica 2/objetos_y_arreglos.js
@@ -26,3 +26,26 @@ const nombresProductos = productosFiltrados.map(producto => producto.nombre);
 console.log(nombresProductos); 
 
 
+
+
+//Ahora con un arreglo de personas, realiza 10 siguiente:
+//1. Usa .find() para buscar a la persona con nombre "Luis".
+//2. Usa .forEach() para imprimir el nombre de cada persona con su edad.
+//3. Usa .reduce() para sumar todas las edades y obtener un total.
+
+const personas = [
+    { nombre: "Ana", edad: 22 },
+    { nombre: "Luis", edad: 35 },
+    { nombre: "Maria", edad: 28 }
+];
+
+//1. Buscar a la persona con nombre "Luis"
+const personaLuis = personas.find(persona => persona.nombre === "Luis");
+console.log(personaLuis);
+//2. Imprimir el nombre de cada persona con su edad
+personas.forEach(persona => {
+    console.log(`Nombre: ${persona.nombre}, Edad: ${persona.edad}`);
+});
+//3. Sumar todas las edades
+const totalEdad = personas.reduce((acumulador, persona) => acumulador + persona.edad, 0);
+console.log (`Total de edades: ${totalEdad}`);
