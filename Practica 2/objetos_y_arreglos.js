b766f6720d8c95f59600450532b98585fce89dc9
EduardoRT1 <122043716@upq.edu.mx>
2025-05-20 12:08:32 -0600

    ejercicio b: Primero separamos los productos que queremos filtrar en una variable, usamos .filter y declaramos que de productos buscamos los mayores a 1000 en costo, luego creamos otra variable para guardar los productos del .map y declaramos que queremos sus nombre con producto.nombre

--- Practica 2/objetos_y_arreglos.js
+++ Practica 2/objetos_y_arreglos.js
@@ -13,3 +13,16 @@ const { nombre, edad, direccion: { ciudad } } = persona;
 console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}`);
 
 
+//1. Filtra los productos cuyo precio sea mayor a 1000.
+//2. Usa .map() para convertir el resultado en un nuevo arreglo con solo los nombres de esos productos.
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 }
+];
+const productosFiltrados = productos.filter(producto => producto.precio > 1000);
+const nombresProductos = productosFiltrados.map(producto => producto.nombre);
+console.log(nombresProductos); 
+
+
