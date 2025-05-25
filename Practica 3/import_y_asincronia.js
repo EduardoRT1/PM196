00f32e0abeb9e65453b9c3ab797805a043085e6b
EduardoRT1 <122043716@upq.edu.mx>
2025-05-25 16:29:41 -0600

    ejercicio b: Creamos la funcion que ejecutará la promesa, en ella declaramos que el usuario con el nombre “admin” recibirá acceso, mientras que cualquier otro parametro dará una denegación

---
+++ Practica 3/import_y_asincronia.js
@@ -0,0 +1,20 @@
+//1. Crea una función verificarUsuari0(usuari0) que retorne una promesa
+//2. Si el nombre de usuario es "admin", la promesa se resuelve con "Acceso concedido", si no,se rechaza con "Acceso denegado"
+
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
+    //usa then y catch para manejar el resultado
+    verificarUsuario("admin")
+        .then(res=> console.log(res)) // acceso concedido
+        .catch(err=> console.error(err));
+
+    verificarUsuario("lalo")
+        .then(res=> console.log(res))
+        .catch(err=> console.error(err)); // acceso denegado
