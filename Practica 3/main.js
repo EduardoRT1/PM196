9bf04cb340f7574d688081b4b85a846052e041bd
EduardoRT1 <122043716@upq.edu.mx>
2025-05-25 16:12:51 -0600

    Practica 3 Ejercicio A: Primero creamos los dos archivos, en utils.js creamos la función y la lógica de restar, al final exportamos dicha función para poder ahora llamarla en main.js, aquí la llamamos y damos los valores para imprimir

---
+++ Practica 3/main.js
@@ -0,0 +1,6 @@
+//I. Crea un archivo llamado utils.js con una función llamada restar(a, b) que retorne la resta de dos números.
+//2. Crea un archivo principal (main.js), importa esa función y úsala ejecutar varias pruebas.
+
+//main.js
+import { restar } from './utils.js';
+console.log(restar(10, 5)); 
---
+++ Practica 3/utils.js
@@ -0,0 +1,9 @@
+//I. Crea un archivo llamado utils.js con una función llamada restar(a, b) que retorne la resta de dos números.
+//2. Crea un archivo principal (main.js), importa esa función y úsala ejecutar varias pruebas.
+
+// utils.js
+function restar(a, b) {
+    return a - b;
+}
+// Exportar la función para que pueda ser utilizada en otros archivos
+export { restar };
