0704107b1174a38f5ac0b7fc04336b53c8cd4bbe
EduardoRT1 <122043716@upq.edu.mx>
2025-06-10 15:38:13 -0600

    Iniciamos con el proyecto blank, conocemos las tres partes que lo conforman y utilizamos nuestro primer componente de texto

--- practica4/App.js
+++ practica4/App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+
+const Texto = () => {
+  return(
+    <Text>Hola mundo!</Text>
+  );
+}
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola mundo!</Text>
+      <Texto></Texto>
+      <Texto></Texto>
+      <Texto></Texto>
       <StatusBar style="auto" />
+      <Button title='Presiona aquí'/>
+      
     </View>
+
   );
 }
 
