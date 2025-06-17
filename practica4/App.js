3f60248ac618d1d0c1c8c5ce8b7ffe851fe00d8c
EduardoRT1 <122043716@upq.edu.mx>
2025-06-16 19:03:16 -0600

    atributos de componentes práctica 6

--- practica4/App.js
+++ practica4/App.js
@@ -4,11 +4,11 @@ import { Button } from 'react-native-web';
 import React, { useState } from 'react';// Importamos React y useState para manejar el estado
 
 // Componente Texto que recibe props y muestra el texto dentro de un componente Text
-const Texto = () => {
+const Texto = ({style}) => {
   const [contenido, setContenido] = useState("Hola Mundo"); // Definimos un estado inicial
   const actualizarTexto = () => {setContenido("State modificado")}; // Función para actualizar el estado
   return(
-    <Text onPress={actualizarTexto}>{contenido}</Text>
+    <Text style={[styles.text,style]} onPress={actualizarTexto}>{contenido}</Text>
   );
 }
 export default function App() {
@@ -16,9 +16,10 @@ export default function App() {
   const actualizaBoton = () => {setTitle("State modificado desde un botón")}; // Función para actualizar el estado del botón
   return (
     <View style={styles.container}>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
+      <StatusBar style="auto" />
+      <Texto style={styles.amarillo}> </Texto>
+      <Texto style={styles.verde}> </Texto>
+      <Texto style={styles.rojo}></Texto>
       <Button onPress={actualizaBoton} title={contenido} />
       <StatusBar style="auto" />
     </View>
@@ -28,7 +29,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-between', //flex-end
+    //flexDirection: 'row',
   },
+  text:{
+    color: 'white',
+    fontSize: 27,
+    width: 200,
+    height: 50,
+  },
+  rojo: {backgroundColor: 'red',  }, //flex: 1,
+  verde: {backgroundColor: 'green',  },
+  amarillo: {backgroundColor: 'yellow', },
+
 });
